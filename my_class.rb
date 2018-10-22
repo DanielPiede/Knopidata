df7324316299a8d7171b7bdf61d04a32b53a1e41
danielpeide <danielpiede@gmail.com>
2018-10-22 10:36:18 +0200

    openHPI

--- my_class.rb
+++ my_class.rb
@@ -1,5 +1,21 @@
 class MyClass
-def test
- puts "hello"
+ extend MethodForward
+
+ forward :geheimnis, to: :secret
+
+ def secret
+  "very secret"
+ end
 end
+
+MyClass.new.geheimnis # => "very secret"
+
+module MethodForward
+ def forward (method_name, to:, **args)
+  if method_defined?(method_name) || method_name.empty? || to.empty? || !instance_method(to) then
+   raise ArgumentError
+  else
+   define_method(method_name, *args) do |args| send(to, *args) end
+  end
+ end
 end
