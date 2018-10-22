2ea7ccd970e86691e4fa1a1749e39d920bb80e76
danielpeide <danielpiede@gmail.com>
2018-10-22 10:41:36 +0200

    openHPI

--- my_class.rb
+++ my_class.rb
@@ -1,3 +1,13 @@
+module MethodForward
+  def forward (method_name, to:)
+    if method_defined?(method_name) || method_name.empty? || to.empty? then
+      raise ArgumentError
+    else
+      define_method(method_name) {send(to)}
+    end
+  end
+end
+
 class MyClass
  extend MethodForward
 
@@ -10,12 +20,3 @@ end
 
 MyClass.new.geheimnis # => "very secret"
 
-module MethodForward
- def forward (method_name, to:, **args)
-  if method_defined?(method_name) || method_name.empty? || to.empty? || !instance_method(to) then
-   raise ArgumentError
-  else
-   define_method(method_name, *args) do |args| send(to, *args) end
-  end
- end
-end
