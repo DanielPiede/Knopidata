013fb41d25cf1d657544842b694cab446b248cc9
danielpiede <danielpiede@gmail.com>
2018-10-28 15:10:17 +0100

    OpenHPI - Umstrukturierung und Test-Datei angelegt

--- Coding/openHPI_ruby_assignment_3_2.rb
+++ Coding/openHPI_ruby_assignment_3_2.rb
@@ -1,9 +1,9 @@
 module MethodForward
-  def forward (method_name, to:)
+  def forward (method_name, *arg, to:)
     if method_defined?(method_name) || method_name.empty? || to.empty? then
       raise ArgumentError
     else
-      define_method(method_name) {send(to)}
+      define_method(method_name) {send(to, arg)}
     end
   end
 end
@@ -13,7 +13,7 @@ class MyClass
 
  forward :geheimnis, to: :secret
 
- def secret
+ def secret()
   "very secret"
  end
 end
